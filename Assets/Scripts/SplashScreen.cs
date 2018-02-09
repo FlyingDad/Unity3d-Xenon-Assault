71d0dc3609b06d75d1f1fe1650468c29a5a9c6d6
Michael <mkroth65@gmail.com>
2018-02-08 21:41:12 -0600

    Added splash screen and created circuit in level1

---
+++ Assets/Scripts/SplashScreen.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SplashScreen : MonoBehaviour {
+
+	[SerializeField] float loadDelay = 3;
+
+	void Awake()
+	{
+		DontDestroyOnLoad(gameObject);
+	}
+	// Use this for initialization
+	void Start () {
+		
+		Invoke("LoadLevel1", loadDelay);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void LoadLevel1(){
+		SceneManager.LoadScene("Level1");
+	}
+}
